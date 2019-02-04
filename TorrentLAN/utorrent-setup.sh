a04ae4a1ce376cb372f05a78a395de45cacfd036
jarleven <jarleven@gmail.com>
2019-02-04 10:42:37 +0100

    Update utorrent-setup.sh
    update -y

--- TorrentLAN/utorrent-setup.sh
+++ TorrentLAN/utorrent-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install libssl1.0.0 libssl-dev -y
 wget http://download-new.utorrent.com/endpoint/utserver/os/linux-x64-ubuntu-13-04/track/beta/ -O utserver.tar.gz
 sudo tar -zxvf utserver.tar.gz -C /opt/
