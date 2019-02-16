a5c1d95e3a3ca8a1dc45066c8144181a677aeb09
jarleven <jarleven@gmail.com>
2019-02-16 16:42:00 +0100

    Update findmac.sh
    Quick cleanup

--- Switch/findmac.sh
+++ Switch/findmac.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+# Array of switches to search through
 ipAddr=("192.168.1.153" "192.168.1.107")
 
 echo "Do you wish to do nmap first?  Script will run [nmap -sP 192.168.1.0/24] "
 
-
-
+# NMAP, to update the host (and switch) ARP table
 select yn in "Yes" "No"; do
     case $yn in
         Yes )nmap -sP 192.168.1.0/24; break;;
@@ -13,6 +13,7 @@ select yn in "Yes" "No"; do
     esac
 done
 
+# Get the MAC address of this ip and convert it to XXXXXX-XXXXXX style as used in the HP swicth.
 echo "Looking for ip $1 in APR table"
 arp -a | grep -i "$1" > logfile.arp
 cat logfile.arp
@@ -22,15 +23,9 @@ sed -i 's/:/-/3' logfile.mac
 sed -i 's/://g' logfile.mac
 
 cat logfile.mac
-
 MACADDR=`cat logfile.mac`
 
-
-
-#IPADDR="192.168.1.153"
-#MACADDR="000c29-e9a1d5"
-
-touch logfile.txt
+# Loop through the switches in the array
 echo "Switch probe log for MAC addr [$MACADDR] IP address [$1]" > logfile.txt
 
 for i in ${!ipAddr[@]};
@@ -44,6 +39,8 @@ do
         cat logfile.tmp | grep -A 4 "Status and Counters" >> logfile.txt
 
 done
+
+# Preset our findings
 echo ""
 echo ""
 echo ""
